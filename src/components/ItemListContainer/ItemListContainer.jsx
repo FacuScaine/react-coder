4ce45707662754beb1a6276931b00c35346b3493
FacuScaine <facusca@gmail.com>
2022-07-16 16:05:34 -0300

    itemCount

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import './ItemListContainer.css';
+import ItemCount from "../ItemCount/ItemCount";
 
 export const ItemListContainer = () => {
+    const onAdd = (cantidad) => {
+        console.log(cantidad)
+    }
     return (
         <>
         <h1>Catalogo</h1>
+        <ItemCount initial={1} stock={5} onAdd={onAdd} />
         </>
 
     );
