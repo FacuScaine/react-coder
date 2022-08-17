994669f2bfe7d4061c09b8470f2e05c20d6106e3
Facundo Scaine <facusca@gmail.com>
2022-08-17 10:35:14 -0300

    cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {addDoc, collection, getFirestore} from 'firebase/firestore';
 import { useCartContext } from "../../contexts/CartContext";
 import { Link } from 'react-router-dom'
 import ItemCart from "../ItemCart/ItemCart.jsx";
@@ -6,6 +7,24 @@ import ItemCart from "../ItemCart/ItemCart.jsx";
 export const Cart = () => {
 
     const { cart, totalPrice } = useCartContext();
+
+    const order = {
+        cliente: {
+            nombre: 'Facundo',
+            email: 'facusca@gmail.com',
+            telefono: '11 36013722',
+            direccion: 'fragata hercules 1579'
+        },
+        items: cart.map(product => ({ id: product.id, titulo: product.title, precio: product.price})),
+        total: totalPrice()
+    }
+
+    const emitirCompra = () =>{
+        const db = getFirestore();
+        const orderCollection = collection(db,'orders');
+        addDoc(orderCollection, order)
+        .then(({id})=>console.log(id))
+    }
 
     if (cart.length === 0) {
         return <>
@@ -22,6 +41,8 @@ export const Cart = () => {
             <p>
                 total:${totalPrice()}
             </p>
+
+            <button onClick={emitirCompra}>Emitir Compra</button>
         </>
     )
 };
